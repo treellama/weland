952f9d2b04fe5426bbf8eb17d22e0eadbc18bc66
treellama <treellama@9080b05e-e1c3-4ab4-8ed1-cafcbe48bdc6>
2009-09-27 01:37:14 +0000

    remember window size and position

---
+++ Settings.cs
@@ -0,0 +1,73 @@
+using System;
+using System.IO;
+using System.Xml;
+
+namespace Weland {
+    // from http://www.codeproject.com/KB/XML/quickndirtyxml.aspx
+    public class Settings {
+	XmlDocument xmlDocument = new XmlDocument();
+	string applicationData;
+	string documentPath;
+	
+	public Settings() {
+	    applicationData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "//Weland";
+	    documentPath = applicationData + "//settings.xml";
+
+	    try {
+		xmlDocument.Load(documentPath);
+	    }
+	    catch {xmlDocument.LoadXml("<settings></settings>");}
+	}
+	
+	public int GetSetting(string xPath, int defaultValue) { 
+	    return Convert.ToInt16(GetSetting(xPath, Convert.ToString(defaultValue))); 
+	}
+	
+	public void PutSetting(string xPath, int value) { 
+	    PutSetting(xPath, Convert.ToString(value)); 
+	}
+
+	public string GetSetting(string xPath,  string defaultValue) { 
+	    XmlNode xmlNode = xmlDocument.SelectSingleNode("settings/" + xPath );
+	    if (xmlNode != null) {
+		return xmlNode.InnerText;
+	    } else { 
+		return defaultValue;
+	    }
+	}
+
+	public void PutSetting(string xPath,  string value) { 
+	    XmlNode xmlNode = xmlDocument.SelectSingleNode("settings/" + xPath);
+	    if (xmlNode == null) { 
+		xmlNode = createMissingNode("settings/" + xPath); 
+	    }
+	    xmlNode.InnerText = value;
+	    try {
+		if (!Directory.Exists(applicationData)) {
+		    Directory.CreateDirectory(applicationData);
+		}
+		xmlDocument.Save(documentPath);
+	    } catch (Exception) {
+	    }
+	}
+      
+	private XmlNode createMissingNode(string xPath) { 
+	    string[] xPathSections = xPath.Split('/');
+	    string currentXPath = "";
+	    XmlNode testNode = null;
+	    XmlNode currentNode = xmlDocument.SelectSingleNode("settings");
+	    foreach (string xPathSection in xPathSections) { 
+		currentXPath += xPathSection;
+		testNode = xmlDocument.SelectSingleNode(currentXPath);
+		if (testNode == null) {
+		    currentNode.InnerXml += "<" + 
+			xPathSection + "></" + 
+			xPathSection + ">";
+		}
+		currentNode = xmlDocument.SelectSingleNode(currentXPath);
+		currentXPath += "/";
+	    }
+	    return currentNode;
+	}
+    }
+}
