82da232ee6fca1499bfc7ca58f93488ca3252488
treellama <treellama@9080b05e-e1c3-4ab4-8ed1-cafcbe48bdc6>
2009-10-10 02:45:05 +0000

    point parameters dialog :C

--- PopupParametersDialogs.cs
+++ PopupParametersDialogs.cs
@@ -38,4 +38,40 @@ namespace Weland {
 	[Widget] CheckButton solid;
 	[Widget] CheckButton transparent;
     }
+
+    public class PointParametersDialog {
+	public PointParametersDialog(Window parent, Level theLevel, short theIndex) {
+	    level = theLevel;
+	    index = theIndex;
+	    Glade.XML gxml = new Glade.XML(null, "pointparameters.glade", "dialog1", null);
+	    gxml.Autoconnect(this);
+	    dialog1.TransientFor = parent;
+	}
+
+	public void Run() {
+	    Point p = level.Endpoints[index];
+	    pointX.Text = String.Format("{0}", p.X);
+	    pointY.Text = String.Format("{0}", p.Y);
+	    dialog1.Run();
+	    
+	    Point n = p;
+	    short i;
+	    if (short.TryParse(pointX.Text, out i)) {
+		n.X = i;
+	    }
+	    if (short.TryParse(pointY.Text, out i)) {
+		n.Y = i;
+	    }
+	    level.Endpoints[index] = n;
+	    dialog1.Destroy();
+	}
+
+	Level level;
+	short index;
+
+	[Widget] Dialog dialog1;
+
+	[Widget] Entry pointX;
+	[Widget] Entry pointY;
+    }
 }
