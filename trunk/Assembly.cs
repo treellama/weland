2594676c466b62e5fc64859f86c05373a3d4fe2a
treellama <treellama@9080b05e-e1c3-4ab4-8ed1-cafcbe48bdc6>
2009-10-21 02:34:50 +0000

    bump to version 1.0.1

--- trunk/Assembly.cs
+++ trunk/Assembly.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly:AssemblyVersionAttribute ("1.0.0.0")]
+[assembly:AssemblyVersionAttribute ("1.0.1.0")]
 [assembly:AssemblyTitleAttribute ("Weland")]
 [assembly:AssemblyDescriptionAttribute ("Marathon Map Editor")]
-[assembly:AssemblyCopyrightAttribute ("© 2009 Gregory Smith (GPL)")]
+[assembly:AssemblyCopyrightAttribute ("\u2117 2009 Gregory Smith (GPL)")]
