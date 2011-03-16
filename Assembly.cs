8ddfdd137ef1c685e125451f79f0af31df9437ee
treellama <treellama@9080b05e-e1c3-4ab4-8ed1-cafcbe48bdc6>
2011-03-16 02:44:28 +0000

    update assembly copyright years

--- Assembly.cs
+++ Assembly.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly:AssemblyVersionAttribute ("1.2.0.0")]
 [assembly:AssemblyTitleAttribute ("Weland")]
 [assembly:AssemblyDescriptionAttribute ("Marathon Map Editor")]
-[assembly:AssemblyCopyrightAttribute ("\u2117 2009-2010 Gregory Smith (GPL)")]
+[assembly:AssemblyCopyrightAttribute ("\u2117 2009-2011 Gregory Smith (GPL)")]
