cce4a9ec216ce05c15995775059b2a6b7b2e290b
Gregory Smith <wolfy@treellama.org>
2018-07-29 21:26:20 -0400

    update version number to 1.4.4

--- Assembly.cs
+++ Assembly.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly:AssemblyVersionAttribute ("1.4.3.0")]
+[assembly:AssemblyVersionAttribute ("1.4.4.0")]
 [assembly:AssemblyTitleAttribute ("Weland")]
 [assembly:AssemblyDescriptionAttribute ("Marathon Map Editor")]
-[assembly:AssemblyCopyrightAttribute ("\u2117 2009-2015 Gregory Smith (GPL)")]
+[assembly:AssemblyCopyrightAttribute ("\u2117 2009-2018 Gregory Smith (GPL)")]
