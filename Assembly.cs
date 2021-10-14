145999252b9fcd902eee03383e9b93f81c1b5b10
Gregory Smith <wolfy@treellama.org>
2021-10-13 22:00:52 -0400

    version 1.4.8

--- Assembly.cs
+++ Assembly.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly:AssemblyVersionAttribute ("1.4.7")]
+[assembly:AssemblyVersionAttribute ("1.4.8")]
 [assembly:AssemblyTitleAttribute ("Weland")]
 [assembly:AssemblyDescriptionAttribute ("Marathon Map Editor")]
 [assembly:AssemblyCopyrightAttribute ("\u2117 2009-2021 Gregory Smith (GPL)")]
