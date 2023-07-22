a91b56118e4b2675ba15c76d25d391f65565642a
Kolfering <benoithauquier@hotmail.com>
2023-07-22 20:44:39 +0200

    Simplify Platform detection code now we are using .net core

--- Weland.cs
+++ Weland.cs
@@ -1,52 +1,14 @@
 using Gtk;
 using System;
-using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Weland
 {
-    // from Mono.TextEditor.Platform
     public static class PlatformDetection
     {
-        static PlatformDetection()
-        {
-            IsWindows = System.IO.Path.DirectorySeparatorChar == '\\';
-            IsMac = !IsWindows && IsRunningOnMac();
-            IsX11 = !IsMac && System.Environment.OSVersion.Platform == PlatformID.Unix;
-        }
-
-        public static bool IsMac { get; private set; }
-        public static bool IsX11 { get; private set; }
-        public static bool IsWindows { get; private set; }
-
-        //From Managed.Windows.Forms/XplatUI
-        static bool IsRunningOnMac()
-        {
-            IntPtr buf = IntPtr.Zero;
-            try
-            {
-                buf = Marshal.AllocHGlobal(8192);
-                // This is a hacktastic way of getting sysname from uname ()
-                if (uname(buf) == 0)
-                {
-                    string os = Marshal.PtrToStringAnsi(buf);
-                    if (os == "Darwin")
-                        return true;
-                }
-            }
-            catch
-            {
-            }
-            finally
-            {
-                if (buf != IntPtr.Zero)
-                    Marshal.FreeHGlobal(buf);
-            }
-            return false;
-        }
-
-        [DllImport("libc")]
-        static extern int uname(IntPtr buf);
+        public static bool IsMac => OperatingSystem.IsMacOS();
+        public static bool IsX11 => Environment.OSVersion.Platform == PlatformID.Unix && !IsMac;
+        public static bool IsWindows => OperatingSystem.IsWindows();
     }
 
     public delegate void ShapesFileChangedEventHandler();
